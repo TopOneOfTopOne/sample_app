a16ae120c32ba8a90017e6ee2fd07a9b89ac6a87
toponeoftopone <nooblingis@gmail.com>
2016-03-18 21:53:21 +0800

    Adding static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Insert Generic title to the left"
+
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Insert Generic title to the left"
+  end
+
+  test 'should get about' do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Insert Generic title to the left"
   end
 
 end
