fa922adac26065a26805cce228d0fe0b09affe38
toponeoftopone <nooblingis@gmail.com>
2016-03-19 11:26:18 +0800

    made changes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @genric_title = "| Insert Generic title to the left"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Insert Generic title to the left"
+    assert_select "title", "Home #{@genric_title}"
 
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Insert Generic title to the left"
+    assert_select "title", "Help #{@genric_title}"
   end
 
   test 'should get about' do
     get :about
     assert_response :success
-    assert_select "title", "About | Insert Generic title to the left"
+    assert_select "title", "About #{@genric_title}"
   end
 
 end
