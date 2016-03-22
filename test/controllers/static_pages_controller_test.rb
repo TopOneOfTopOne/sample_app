fcf00168dab51a004fb8d46cdcedee9377ab93c6
anon <anon@cyber.net>
2016-03-22 21:45:33 +0800

    refactor css

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@genric_title}"
   end
 
+  test 'should get contact' do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contacct | #{@genric_title}"
+  end
+
 end
