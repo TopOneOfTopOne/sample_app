e354168212f7beb095d0119079d854a6602f411e
toponeoftopone <nooblingis@gmail.com>
2016-04-02 17:18:48 +0800

    Refactor + fix code for relationship controller
    made code idiomatically correct

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,14 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-    user_to_follow_id = params[:followed_id]
-    Relationship.create(follower_id: current_user.id, followed_id: user_to_follow_id)
-    render 'users/show'
+    user_to_follow = User.find(params[:followed_id])
+    current_user.follow(user_to_follow)
+    redirect_to user_to_follow
   end
 
   def destroy
-    user_to_unfollow_id = params[:followed_id]
-    Relationship.destroy(follower_id: current_user.id, followed_id: user_to_unfollow_id)
-    render 'users/show'
+    user_to_unfollow = Relationship.find(params[:id]).followed
+    current_user.unfollow(user_to_unfollow)
+    redirect_to user_to_unfollow
   end
 end
