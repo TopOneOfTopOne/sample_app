93d146749fb3e1dd0da413b0f0e7173a36782fba
toponeoftopone <nooblingis@gmail.com>
2016-04-02 16:35:18 +0800

    add test
    Tests verify only logged in users can create or destroy elements in
    Relationships table

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,12 @@
 class RelationshipsController < ApplicationController
+  def create
+    user_to_follow_id = params[:followed_id]
+    Relationship.create(follower_id: current_user.id, followed_id: user_to_follow_id)
+    render user_path(user_to_follow_id)
+  end
+
+
+  def destroy
+
+  end
 end
--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:jack)
+  end
+
+  test "redirect when creating if not logged in" do
+    assert_no_difference 'Relationship.count' do
+      post :create
+      assert_redirected_to login_url
+    end
+  end
+
+  test "redirect when destroying if not logged in" do
+    assert_no_difference 'Relationship.count' do
+      delete :destroy
+      assert_redirected_to login_url
+    end
+  end
 end
